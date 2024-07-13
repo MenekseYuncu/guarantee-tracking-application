4275bb6125c22f5c7827e05a51eaf01814f6b32e
MenekseYuncu <baioretto66@gmail.com>
2024-07-13 23:39:38 +0300

    Initial commit

---
+++ src/main/java/org/menekseyuncu/guaranteetrackingapplication/GuaranteeTrackingApplication.java
@@ -0,0 +1,13 @@
+package org.menekseyuncu.guaranteetrackingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaranteeTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuaranteeTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/menekseyuncu/guaranteetrackingapplication/GuaranteeTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.menekseyuncu.guaranteetrackingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuaranteeTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
