f36178ea45989d314830f6801092dfd8182ea78f
MenekseYuncu <baioretto66@gmail.com>
2024-07-14 23:57:19 +0300

    database created

---
+++ src/main/resources/db/migration/V1__initialize_database.sql
@@ -0,0 +1,33 @@
+create table if not exists device
+(
+    id            bigserial
+        constraint pk__device__id primary key,
+    serial_number varchar(15)  not null
+        constraint u__device__serial_number unique,
+    brand         varchar(150) not null,
+    model         varchar(200) not null,
+    created_at    timestamp(0) not null,
+    updated_at    timestamp(0)
+);
+
+-- Warranty tablosu
+create table if not exists warranty
+(
+    id              bigserial
+        constraint pk__warranty__id primary key,
+    device_id       bigint
+        constraint fk__warranty__device__id references device (id),
+    purchase_date   date         not null,
+    warranty_status varchar(20)  not null
+        constraint c__warranty_status__status check (warranty_status in ('ACTIVE', 'EXPIRED', 'PENDING')),
+    created_at      timestamp(0) not null,
+    updated_at      timestamp(0)
+);
+
+insert into device (serial_number, brand, model, created_at)
+values ('123456789012344', 'brand', 'phone', current_timestamp),
+       ('123456789012361', 'brand', 'tab', current_timestamp);
+
+insert into warranty (device_id, purchase_date, warranty_status, created_at)
+values (1, '2022-07-01', 'ACTIVE', current_timestamp),
+       (2, '2021-06-15', 'EXPIRED', current_timestamp);
