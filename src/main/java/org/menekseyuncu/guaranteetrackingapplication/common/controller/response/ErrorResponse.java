052578baf044b8061c56f6482e0303d6c9e1c97b
MenekseYuncu <baioretto66@gmail.com>
2024-07-22 23:54:14 +0300

    Base error response added

---
+++ src/main/java/org/menekseyuncu/guaranteetrackingapplication/common/controller/response/ErrorResponse.java
@@ -0,0 +1,17 @@
+package org.menekseyuncu.guaranteetrackingapplication.common.controller.response;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse extends BaseResponse<String> {
+
+    public ErrorResponse(HttpStatus httpStatus, String message) {
+        super(LocalDateTime.now(), httpStatus, false, message);
+    }
+
+    public static ErrorResponse failureOf(HttpStatus httpStatus, String message) {
+        return new ErrorResponse(httpStatus, message);
+    }
+}
+
