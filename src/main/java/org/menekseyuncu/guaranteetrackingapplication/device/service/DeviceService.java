92e31a4c4f5b5a301480325d25d57b769a874eb1
MenekseYuncu <baioretto66@gmail.com>
2024-07-24 14:51:20 +0300

    update and delete methods added

--- src/main/java/org/menekseyuncu/guaranteetrackingapplication/device/service/DeviceService.java
+++ src/main/java/org/menekseyuncu/guaranteetrackingapplication/device/service/DeviceService.java
@@ -1,9 +1,32 @@
 package org.menekseyuncu.guaranteetrackingapplication.device.service;
 
 import org.menekseyuncu.guaranteetrackingapplication.device.controller.request.DeviceCreateRequest;
+import org.menekseyuncu.guaranteetrackingapplication.device.controller.request.DeviceUpdateRequest;
 
+/**
+ * Service interface for managing devices.
+ */
 public interface DeviceService {
 
+    /**
+     * Creates a new device.
+     *
+     * @param createRequest the request containing the device details
+     */
     void createDevice(DeviceCreateRequest createRequest);
 
+    /**
+     * Updates an existing device.
+     *
+     * @param id            the ID of the device to update
+     * @param updateRequest the request containing updated device details
+     */
+    void updateDevice(Long id, DeviceUpdateRequest updateRequest);
+
+    /**
+     * Deletes an existing device.
+     *
+     * @param id the ID of the device to delete
+     */
+    void deleteDevice(Long id);
 }
